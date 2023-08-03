253c76a162f90909332ee59ab29749899c4b2522
PRADHUMAN KUMAR <121790103+pk4727@users.noreply.github.com>
2023-08-03 23:15:25 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-import pyzbar
-# from pyzbar import cv2 ,decode
-# def barcodeReader(image):
-#     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     barcodes = decode(gray_img)
-
-# barcode = barcodeReader("My_image")
-# print (barcode)
